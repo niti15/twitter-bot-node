348e86d6e24424709692a7cbea338af4f5d685ad
Nithya Sugumar <nithya@yelobank.in>
2020-08-11 05:28:30 +0530

    removed

--- config.js
+++ config.js
@@ -1,21 +1,15 @@
 require("dotenv").config();
 
-const dbUser = process.env.DB_USER || "nithya";
-const dbPass = process.env.DB_PASS || "12345678N";
-const dbName = process.env.DB_NAME || "twitterbot";
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+const dbName = process.env.DB_NAME;
 
 const mongoDBUrl = `mongodb://${dbUser}:${dbPass}@dbh11.mlab.com:27117/${dbName}`;
 const twitterConfig = {
-  consumer_key: process.env.CONSUMER_KEY || "llmenRC0HExKpDcpHPUArUsvV",
-  consumer_secret:
-    process.env.CONSUMER_SECRET ||
-    "24uIoBvvHcmat6XkyMHkbBtp0p4LYgfwQypReHIJmzKrDcSV2g",
-  access_token_key:
-    process.env.ACCESS_TOKEN ||
-    "746656089937084416-hE5mVltPDrw0cmoD8ufCRumItpG4ggW",
-  access_token_secret:
-    process.env.ACCESS_TOKEN_SECRET ||
-    "T2fWeJTynTf1SLSVomBlJd2cFDDEvuBTEefEjk4kcNPkm",
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 };
 
 module.exports = {
